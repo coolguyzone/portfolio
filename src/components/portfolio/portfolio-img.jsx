ff3baf85f3068b93d3a9bf50b9a7413d88bf2aa5
Alexander Krawiec <akrawiec@fb.com>
2017-07-03 09:11:36 -0700

    added imgs to portfolio

---
+++ src/components/portfolio/portfolio-img.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PortfolioIMG = (props) => (
+  <div className="portfolio-img col-sm-4">
+    <img src={props.url} />
+  </div>
+);
+
+export default PortfolioIMG;
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './portfolio.css';
 import TitleBar from '../title-bar/title-bar';
+import PortfolioIMG from './portfolio-img';
 
 const Portfolio = () => (
   <div id="portfolio">
     <TitleBar title="Portfolio" />
+    <div className="row">
+      <div id="portfolio-gallery" className="col-sm-10 col-sm-offset-1">
+        <div className="row">
+          <PortfolioIMG url={require('../../img/level-water.png')} />
+          <PortfolioIMG url={require('../../img/lazy-bartender.png')} />
+          <PortfolioIMG url={require('../../img/sup-seasonal.png')} />
+          <PortfolioIMG url={require('../../img/what-do-we-eat.png')} />
+          <PortfolioIMG url={require('../../img/chazz.png')} />
+        </div>
+      </div>
+    </div>
   </div>
+
 );
 
 export default Portfolio;
