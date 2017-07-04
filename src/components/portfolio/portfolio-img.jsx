5ed2acb3df8f2bbf76e3345579886661671cbdb4
Alexander Krawiec <akrawiec@fb.com>
2017-07-03 17:21:48 -0700

    work on portfolio imgs

--- src/components/portfolio/portfolio-img.jsx
+++ src/components/portfolio/portfolio-img.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const PortfolioIMG = (props) => (
-  <div className="portfolio-img col-sm-4">
-    <img src={props.url} />
+  <div className="col-sm-4">
+    <div className="portfolio-img" style={props.stile}>
+      <div>
+        <h2>levelwater.io</h2>
+        <p>A Full-stack app to help small water systems manage their infrastructure and set water rates.</p>
+        <p>Main tech used: React, Redux, Express, Node, C3</p>
+      </div>
+    </div>
+    {/* <img src={props.url} /> */}
   </div>
 );
 
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -9,11 +9,29 @@ const Portfolio = () => (
     <div className="row">
       <div id="portfolio-gallery" className="col-sm-10 col-sm-offset-1">
         <div className="row">
-          <PortfolioIMG url={require('../../img/level-water.png')} />
-          <PortfolioIMG url={require('../../img/lazy-bartender.png')} />
-          <PortfolioIMG url={require('../../img/sup-seasonal.png')} />
-          <PortfolioIMG url={require('../../img/what-do-we-eat.png')} />
-          <PortfolioIMG url={require('../../img/chazz.png')} />
+          <PortfolioIMG
+            //  url={require('../../img/level-water.png')}
+            stile={{background: "url('level-water.png')"}}
+
+         />
+          <PortfolioIMG
+            //  url={require('../../img/lazy-bartender.png')}
+            stile={{background: "url('lazy-bartender.png')"}}
+           />
+          <PortfolioIMG
+            //  url={require('../../img/sup-seasonal.png')}
+            stile={{background: "url('sup-seasonal.png')"}}
+           />
+         </div>
+         <div className="row">
+          <PortfolioIMG
+            //  url={require('../../img/what-do-we-eat.png')}
+            stile={{background: "url('what-do-we-eat.png')"}}
+           />
+          <PortfolioIMG
+            //  url={require('../../img/chazz.png')}
+            stile={{background: "url('chazz.png')"}}
+           />
         </div>
       </div>
     </div>
