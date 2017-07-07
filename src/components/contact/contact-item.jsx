d1297b9d675e40cea6d7af65709685f196720eeb
Alexander Krawiec <akrawiec@fb.com>
2017-07-06 17:35:15 -0700

    more work on contact

--- src/components/contact/contact-item.jsx
+++ src/components/contact/contact-item.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const ContactItem = (props) => (
+  <a href={props.url} target="_blank">
   <div className="contact-item col-sm-5">
     <h3>{props.header}</h3>
-    <a href={props.url} target="_blank"><p>{props.link}</p></a>
+    <p>{props.link}</p>
   </div>
+  </a>
 );
 
 export default ContactItem;
