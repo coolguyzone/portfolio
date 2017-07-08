882f05058da2274f022bbc2695bb12a316b6d447
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-07-08 13:30:22 -0700

    finished basic mobile styles

--- src/components/bio/experience/experience-item.jsx
+++ src/components/bio/experience/experience-item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ExperienceItem = (props) => (
   <div className="experience-item col-sm-6 col-xs-12">
     <div className="row">
-      <div className="col-sm-2 col-xs-2 square-parent" >
+      <div className="col-sm-2 col-xs-3 square-parent" >
         <div className="experience-square" style={{'background-color': props.bg}}></div>
 
       </div>
@@ -11,7 +11,7 @@ const ExperienceItem = (props) => (
         <h3>{props.title}</h3>
         <p>{props.dates}</p>
       </div>
-      <div className="experience-company col-sm-5 col-xs-5">
+      <div className="experience-company col-sm-5 col-xs-4">
         <h3>{props.company}</h3>
         <p>{props.url}</p>
       </div>
