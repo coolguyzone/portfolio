c6baf0cb56a39bdf4ad063734fdbb998404051b9
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-07-08 09:01:13 -0700

    added media styles for header

---
+++ src/components/heading/heading-mobile-text.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HeadingMobileText = () => (
+  <div id="heading-mobile-text">
+    <div>
+      <h2>Alex</h2>
+    </div>
+    <div>
+      <h2>Krawiec</h2>
+    </div>
+  </div>
+);
+
+export default HeadingMobileText;
--- src/components/heading/heading.jsx
+++ src/components/heading/heading.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './heading.css';
 import HeadingText from './heading-text';
 import HeadingButton from './heading-button';
+import HeadingMobileText from './heading-mobile-text';
 
 const Heading = () => (
   <div id="heading">
+    <HeadingMobileText />
     <HeadingText />
     <HeadingButton />
   </div>
